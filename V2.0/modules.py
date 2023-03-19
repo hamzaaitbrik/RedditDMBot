542e4003f791d220f839b48384ffcf4852d15d23
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-03-19 12:13:10 +0000

    Update modules.py

--- V2.0/modules.py
+++ V2.0/modules.py
@@ -1,9 +1,22 @@
-message = '' # message to send to Reddit users
-keywords = [] # list of keywords to scrape Reddit usernames based on. The bot will check these keywords and scrape a list of Reddit usernames that made a comment using them.
+from XPATH import *
+from CONSTANTS import *
+from variables import *
+from account import account
+import csv
+import requests
+import json
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+from time import sleep
+from random import uniform,randint
 
 
-data = [] # ! Do not touch, the bot will handle.
-scrape_urls = [] # ! Do not touch, the bot will handle.
-list_usernames = [] # ! Do not touch, the bot will handle.
-usernames_sent = [] # ! Do not touch, the bot will handle.
-dict_usernames = {} # ! Do not touch, the bot will handle.
+
+
+def log(data):
+        print(data)
+        with open('log.txt', 'a') as log:
+            log.write(f'{data}\n')
