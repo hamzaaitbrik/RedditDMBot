16b0a51a9f43079f755058c32723e5e83ea82068
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-03-19 12:14:07 +0000

    Create

---
+++ V2.0/XPATH.py
@@ -0,0 +1,12 @@
+REDDIT_LOGIN_PAGE = {
+    'usernameInput':'//*[@id="loginUsername"]',
+    'passwordInput':'//*[@id="loginPassword"]',
+    'loginButton':'//fieldset[5]/button'
+}
+REDDIT_CHAT_PAGE = {
+    'usernameInput':'//div[1]/span/input',
+    'firstResult':'//div[2]/div[2]/div[1]',
+    'startChatButton':'//form//button[2]',
+    'messageInput':'//textarea',
+    'sendButton':'//form//button'
+}
