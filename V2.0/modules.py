49b9d1b1d31437e19933e9eb8153cf3bdb022ca3
Hamza <36282849+hamzaaitbrik@users.noreply.github.com>
2023-04-11 14:43:59 +0000

    bug fixes

--- V2.0/modules.py
+++ V2.0/modules.py
@@ -9,6 +9,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from datetime import datetime
 from time import sleep
 from random import uniform,randint
@@ -20,3 +21,12 @@ def log(data):
         print(data)
         with open('log.txt', 'a') as log:
             log.write(f'{data}\n')
+
+def checkExistByXPATH(xpath):
+    try:
+        #sleep(uniform(3.5,5))
+        #WebDriverWait(driver,5).until(EC.presence_of_element_located((By>
+        driver.find_element(By.XPATH,xpath)
+    except NoSuchElementException:# or TimeoutException:
+        return False
+    return True
