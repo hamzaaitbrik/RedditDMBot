e0119e83168727ad68b5721aba3b16eae2020ad1
hamzaaitbrik <kirbtia@gmail.com>
2024-01-14 16:34:31 +0100

    better working structure

--- CONSTANTS.py
+++
@@ -1,3 +0,0 @@
-REDDIT_LOGIN_PAGE_URL = r'https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2Fuser%2F'
-API_URL = r'https://api.pushshift.io/reddit/search/comment/?q='
-MESSAGE_URL = r'https://chat.reddit.com/user'
---
+++ src/constants.py
@@ -0,0 +1,2 @@
+REDDIT_LOGIN_PAGE_URL = r'https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2Fuser%2F'
+MESSAGE_URL = r'https://chat.reddit.com/user'
