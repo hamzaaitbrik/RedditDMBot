356d01642568008b830203acb2e10dcdd3ae4235
hamzaaitbrik <kirbtia@gmail.com>
2024-02-14 18:30:33 +0100

    junk deletion

--- test/rsrc/js/chat_message.enable.js
+++
@@ -1,1 +0,0 @@
-document.querySelector("body > faceplate-app > rs-app").shadowRoot.querySelector("div.container > rs-direct-chat").shadowRoot.querySelector("section > rs-message-composer").shadowRoot.querySelector("form > button.button-send.button-medium.px-\\[length\\:var\\(--rem8\\)\\].button-plain.icon.button.flex.items-center.justify-center").disabled = false;
