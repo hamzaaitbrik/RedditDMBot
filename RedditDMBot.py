b8bc2ce8e301e323751908f2d209c508e3df168d
hamzaaitbrik <kirbtia@gmail.com>
2024-02-15 15:59:26 +0100

    further development

--- RedditDMBot.py
+++ RedditDMBot.py
@@ -10,7 +10,7 @@ list_usernames, usernames_sent = list(), list()
 
 
 
-async def RedditDMBot(account,username):
+async def RedditDMBot(accounts,used_accounts,account,username):
     async with async_playwright() as playwright:
         device = playwright.devices['Desktop Chrome']
         if(config['proxy']['proxy'] == 'localhost'):
@@ -43,7 +43,7 @@ async def RedditDMBot(account,username):
             await page.locator(locators['sendButtonLocator']).click()
             sleep(1)
             try:
-                page.locator(locators['unableToDM'])
+                page.locator(locators['unableToDMLocator'])
                 log(f'[Main] {account["username"]} was unable to send DM. Writing it to the database...')
                 writeToCSV(
                     paths['toss_accounts'],
@@ -52,6 +52,8 @@ async def RedditDMBot(account,username):
                         account['password']
                     ]
                 )
+                accounts.remove(account)
+                await page.screenshot( path=f"screenshots/failed/{account['username']}_to_{username}|{page.locator(locators['authorLocator']).get_attribute('title')}.png" )
             except:
                 log(f'[Main] Message sent to {username} using {account["username"]}. Writing it to the database...')
                 writeToCSV(
@@ -61,6 +63,8 @@ async def RedditDMBot(account,username):
                         account['username']
                     ]
                 )
+                await page.screenshot( path=f"screenshots/succeeded/{account['username']}_to_{username}|{page.locator(locators['authorLocator']).get_attribute('title')}.png" )
+                used_accounts.append(account)
             await page.screenshot( path=f"screenshots/{account['username']}_to_{username}.png" )
         except:
             log(f'[Main] ERROR! An exception occured while trying to DM {username} using {account["username"]}:{account["password"]}.')
@@ -92,9 +96,8 @@ def main():
         if(len(accounts) == 0): # to check if all accounts are used
             accounts, used_accounts = used_accounts, list() # repopulates accounts with used_accounts and reinitialize used_accounts to an empty list
         account = accounts.pop(0) # getting the first account of the list accounts, then removing it
-        asyncio.run(RedditDMBot(account,username)) # entry point
+        asyncio.run(RedditDMBot(accounts,used_accounts,account,username)) # entry point
         list_usernames.remove(username) # removing that username from the list of usernames to DM
-        used_accounts.append(account) # appending the account used to DM to the list of used accounts
 
 
 
