83bf58f52d94e176be5916c15d94e50902e30088
hamzaaitbrik <kirbtia@gmail.com>
2025-04-21 21:37:52 +0100

    adding modules

--- modules.py
+++ modules.py
@@ -2,7 +2,7 @@ from requests import get
 from csv import reader, writer
 from json import load, loads
 import asyncio
-import nodriver
+from . import zendriver
 from datetime import datetime
 from time import sleep
 from random import uniform, randint, choice
