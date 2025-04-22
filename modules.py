339f84a11253e048c4992876f68a39ed962a9d84
hamzaaitbrik <kirbtia@gmail.com>
2025-04-22 22:23:59 +0100

    modules

--- modules.py
+++ modules.py
@@ -2,7 +2,7 @@ from requests import get
 from csv import reader, writer
 from json import load, loads
 import asyncio
-from . import zendriver
+import zendriver
 from datetime import datetime
 from time import sleep
 from random import uniform, randint, choice
